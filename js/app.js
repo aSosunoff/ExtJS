545c7e076ec028a0514b3cf3cf9c7eff93a90218
aSosunoff <al.sosunoff@gmail.com>
2016-02-11 20:50:41 +0300

    php

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 /**
  * Created by SOSUNOV_AI on 28.01.2016.
  */
-//Ext.application({
-//    name: 'HelloExt',
-//    launch: function() {
-//        Ext.create('Ext.container.Viewport', {
-//            layout: 'fit',
-//            items: [
-//                {
-//                    title: 'Приложение на Ext JS 4',
-//                    html : '<h3>Добро пожаловать в мир Ext JS 4!</h3>'
-//                }
-//            ]
-//        });
-//    }
-//});
+Ext.application({
+    name: 'HelloExt',
+    launch: function() {
+        Ext.create('Ext.container.Viewport', {
+            layout: 'fit',
+            items: [
+                {
+                    title: 'Приложение на Ext JS 4',
+                    html : '<h3>Добро пожаловать в мир Ext JS 4!</h3>'
+                }
+            ]
+        });
+    }
+});
 
 //Ext.onReady(function(){
 //    Ext.create('Ext.container.Viewport', {
